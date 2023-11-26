3e6a8605eb8a17cc01ef681c41f8967252daf688
Vaibhav <vaibhavverma21122001@gmai.com>
2023-11-26 21:48:53 +0530

    second

---
+++ Questions/Array/Missing_and_repeating_number.cpp
@@ -0,0 +1,58 @@
+// Question - https://www.codingninjas.com/studio/problems/missing-and-repeating-numbers_873366?leftPanelTabValue=PROBLEM
+
+// Method 1 - Using map TC = O(N), SC = O(N)
+
+
+// Method 2 - TC = O(N), SC = O(1)
+// visited node - multiply with -1
+
+
+#include <bits/stdc++.h>
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	// // Write your code here 
+	// int tsum = (n*(n+1))/2;
+	// int arrsum = accumulate(arr.begin(),arr.end(),0);
+
+	int dup;
+	for(int i = 0 ; i < n ;i++){
+		int index = abs(arr[i]) - 1;
+		if(arr[index] < 0 ){
+			// cout<<"duplicate is : "<<abs(arr[i])<<endl;
+			dup = abs(arr[i]);
+			break;
+		}
+		else{
+			arr[index]*= -1;
+		}
+	}
+
+
+	// To find missing, but array is changed now
+
+	int mis;
+    	for(int i = 0 ; i < n ;i++){
+    		int index = abs(arr[i]) - 1;
+    		if(arr[index] < 0 ){
+    			continue;
+    		}
+    		else{
+    			arr[index]*= -1;
+    		}
+    	}
+    
+    	for(int i = 0 ; i < n; i++){
+    		if(arr[i] > 0){
+    		    mis = i+1;
+				break;
+    		}
+    	}
+    	
+	
+
+
+	// int mis = tsum + dup - arrsum;
+	return {mis , dup};
+	
+}
---
+++ Questions/Array/Time_to_collect_garbage.cpp
@@ -0,0 +1,48 @@
+// Question - https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/description/
+// Solution - https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/solutions/4326214/simple-easy-solution-with-comments/
+
+
+class Solution {
+public:
+    // Logic - 
+    // total time = total no. of M, P , G + time to reach last house where there is P,G,M
+
+    int garbageCollection(vector<string>& g, vector<int>& t) {
+        int total = 0, last_G = 0 , last_P = 0, last_M = 0;
+
+        for(int i = 0 ; i < g.size(); i++){
+            for(int j = 0 ; j < g[i].length(); j++){
+                if(g[i][j] == 'G'){
+                    last_G = i;
+                }
+                else if(g[i][j] == 'P'){
+                    last_P = i;
+                }
+                else{
+                    last_M = i;
+                }
+                total++; // 1 minute for each M,P,G
+            }
+        }
+
+        // Prefix sum
+        for(int i = 1 ; i < t.size(); i++){
+            t[i] = t[i] + t[i-1];
+        }
+
+        // time for each of them to reach ith home
+        if(last_G != 0){
+            total = total + t[last_G - 1];
+        }
+        
+        if(last_P != 0){
+            total = total + t[last_P - 1];
+        }
+
+        if(last_M != 0){
+            total = total + t[last_M - 1];
+        }
+
+        return total;
+    }
+};
